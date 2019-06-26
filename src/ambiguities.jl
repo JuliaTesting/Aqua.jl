3036af364db98681b9a194dd8feecb6a438031a4
Takafumi Arakaki <aka.tkf@gmail.com>
2019-06-26 14:52:28 -0700

    Add exclude kwarg to test_ambiguities

--- src/ambiguities.jl
+++ src/ambiguities.jl
@@ -32,9 +32,41 @@ ispackage(m::Module) =
         parentmodule(m) == m
     end
 
+strnameof(x) = string(x)
+strnameof(x::Type) = string(nameof(x))
+
+normalize_exclude(x::String) = x
+normalize_exclude(x::Union{Type, Function}) =
+    join((fullname(parentmodule(x))..., strnameof(x)), ".")
+normalize_exclude(::Any) =
+    error("Only a function and type can be excluded.")
+
+function getobj(spec::String, modules)
+    nameparts = Symbol.(split(spec, "."))
+    for m in modules
+        if nameparts[1] === nameof(m)
+            return foldl(getproperty, nameparts[2:end], init=m)
+        end
+    end
+    error("Object $spec not found in following modules:\n$modules")
+end
+
+function normalize_and_check_exclude(exclude::AbstractVector, packages)
+    strexclude = mapfoldl(normalize_exclude, push!, exclude, init=String[])
+    modules = map(Base.require, packages)
+    for (str, obj) in zip(strexclude, exclude)
+        obj isa String && continue
+        if getobj(str, modules) !== obj
+            error("Name `$str` is resolved to a different object.")
+        end
+    end
+    return strexclude :: Vector{String}
+end
+
 function _test_ambiguities(
     packages::Vector{PkgId};
     color::Union{Bool, Nothing} = nothing,
+    exclude::AbstractArray = [],
     # Options to be passed to `Test.detect_ambiguities`:
     recursive::Bool = true,
     imported::Bool = false,
@@ -46,13 +78,18 @@ function _test_ambiguities(
         imported = imported,
         ambiguous_bottom = ambiguous_bottom,
     ))
+    exclude_repr = repr(normalize_and_check_exclude(exclude, packages))
 
     # Ambiguity test is run inside a clean process.
     # https://github.com/JuliaLang/julia/issues/28804
     code = """
     $(Base.load_path_setup_code())
     using Aqua
-    Aqua.test_ambiguities_impl($packages_repr, $options_repr) || exit(1)
+    Aqua.test_ambiguities_impl(
+        $packages_repr,
+        $options_repr,
+        $exclude_repr,
+    ) || exit(1)
     """
     cmd = Base.julia_cmd()
     if something(color, Base.JLOptions().color == 1)
@@ -83,10 +120,25 @@ function reprpkgid(pkg::PkgId)
     return "Base.PkgId(Base.UUID($(repr(uuid))), $(repr(name)))"
 end
 
-function test_ambiguities_impl(packages::Vector{PkgId}, options::NamedTuple)
+getobj(m::Method) = getproperty(m.module, m.name)
+
+function test_ambiguities_impl(
+    packages::Vector{PkgId},
+    options::NamedTuple,
+    exclude::Vector{String},
+)
     modules = map(Base.require, packages)
     @debug "Testing method ambiguities" modules
     ambiguities = detect_ambiguities(modules...; options...)
+
+    if !isempty(exclude)
+        exclude_objs = getobj.(exclude, Ref(modules))
+        ambiguities = filter(ambiguities) do (m1, m2)
+            # `getobj(m1) == getobj(m2)` so no need to check `m2`
+            getobj(m1) ∉ exclude_objs
+        end
+    end
+
     if !isempty(ambiguities)
         printstyled("$(length(ambiguities)) ambiguities found", color=:red)
         println()
