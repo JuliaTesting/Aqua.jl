8339e355648609a3bc0ffa43f033847b434ffa11
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-07 13:53:42 -0700

    Make test_all configurable with flags (#35)
    There are a lot of repetition across packages for running optional
    tests.  Let's avoid this by including all tests in `test_all` and
    turning on/off by flags.

--- test/test_smoke.jl
+++ test/test_smoke.jl
@@ -2,14 +2,13 @@ module TestSmoke
 
 using Aqua
 Aqua.test_all(Aqua)
-
-using Test
-@testset "test_stale_deps" begin
-    Aqua.test_stale_deps(Aqua)
-end
-
-@testset "test_deps_compat" begin
-    Aqua.test_deps_compat(Aqua)
-end
+Aqua.test_all(
+    Aqua;
+    ambiguities = false,
+    unbound_args = false,
+    undefined_exports = false,
+    stale_deps = true,
+    deps_compat = true,
+)
 
 end  # module
