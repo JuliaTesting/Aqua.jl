c8d3655b04f44aa90a5e36c969943b5398e1471b
Takafumi Arakaki <aka.tkf@gmail.com>
2021-09-28 21:02:15 -0400

    Better printing for unbound args

--- src/unbound_args.jl
+++ src/unbound_args.jl
@@ -4,10 +4,11 @@
 Test that all methods in `module` and its submodules do not have
 unbound type parameter.
 """
-function test_unbound_args(m::Module)
+function test_unbound_args(m::Module; quiet::Bool = false)
     # https://github.com/JuliaLang/julia/pull/31972
     # @test detect_unbound_args(m; recursive=true) == []
-    @test detect_unbound_args_recursively(m) == []
+    io = quiet ? devnull : stdout
+    @test detect_unbound_args_recursively(io, m) == []
 end
 
 function detect_unbound_args_recursively(m)
@@ -16,4 +17,29 @@ function detect_unbound_args_recursively(m)
         append!(methods, detect_unbound_args(x))
     end
     return methods
+end
+
+function detect_unbound_args_recursively(io::IO, m)
+    methods = detect_unbound_args_recursively(m)
+    print_unbound_args(io, methods)
+    return methods
+end
+
+function print_unbound_args(io::IO, methods)
+    buf = IOBuffer()
+    for (i, mth) in enumerate(methods)
+        println(io, "Unbound arg #", i)
+
+        show(buf, mth)
+        str = String(take!(buf))
+        mtch = match(r"^(.*) at (.*\.jl:[0-9]+)", str)
+        if mtch === nothing
+            println(io, str)
+        else
+            println(io, mtch[1], " at:")
+            printstyled(io, mtch[2]; bold = true)
+            println(io)
+        end
+        println(io)
+    end
 end
