c8a479774e5d39198f2305e982ebb759e3a25015
Dilum Aluthge <dilum@aluthge.com>
2023-05-31 17:14:15 -0400

    Add the `ignore` kwarg to the `Aqua.test_deps_compat` function

--- src/deps_compat.jl
+++ src/deps_compat.jl
@@ -1,21 +1,24 @@
 """
-    Aqua.test_deps_compat(package)
+    Aqua.test_deps_compat(package; [ignore])
 
 Test that `Project.toml` of `package` list all `compat` for `deps`.
 
 # Arguments
 - `packages`: a top-level `Module`, a `Base.PkgId`, or a collection of
   them.
+
+# Keyword Arguments
+- `ignore::Vector{Symbol}`: names of dependent packages to be ignored.
 """
 test_deps_compat
-function test_deps_compat(packages)
-    @testset "$(result.label)" for result in analyze_deps_compat(packages)
+function test_deps_compat(packages; kwargs...)
+    @testset "$(result.label)" for result in analyze_deps_compat(packages; kwargs...)
         @debug result.label result
         @test result ⊜ true
     end
 end
 
-analyze_deps_compat(packages) = [_analyze_deps_compat_1(pkg) for pkg in aspkgids(packages)]
+analyze_deps_compat(packages; ignore) = [_analyze_deps_compat_1(pkg; kwargs...) for pkg in aspkgids(packages) if !(Symbol(p.name) in ignore)]
 
 function _analyze_deps_compat_1(pkg::PkgId)
     result = root_project_or_failed_lazytest(pkg)
