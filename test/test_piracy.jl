f1f32e715d6057afce0e2f9b2bef9ad4b74af1c4
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-06-16 16:40:52 +0200

    Add tests for piracy kwarg `treat_as_own`

--- test/test_piracy.jl
+++ test/test_piracy.jl
@@ -1,4 +1,8 @@
+push!(LOAD_PATH, joinpath(@__DIR__, "pkgs", "PiracyForeignProject"))
+
 baremodule PiracyModule
+
+using PiracyForeignProject: ForeignType, ForeignParameterizedType
 
 using Base:
     Base,
@@ -28,6 +32,7 @@ Base.findlast(::Set{Foo}, x::Int) = x + 1
 Base.findlast(::Type{Val{Foo}}, x::Int) = x + 1
 Base.findlast(::Tuple{Vararg{Bar{Set{Int}}}}, x::Int) = x + 1
 Base.findlast(::Val{:foo}, x::Int) = x + 1
+Base.findlast(::ForeignParameterizedType{Foo}, x::Int) = x + 1
 
 # Not piracy
 const MyUnion = Union{Int,Foo}
@@ -40,12 +45,25 @@ Base.findfirst(::Set{Vector{Char}}, ::Int) = 1
 Base.findfirst(::Union{Foo,Bar{Set{Unsigned}},UInt}, ::Tuple{Vararg{String}}) = 1
 Base.findfirst(::AbstractChar, ::Set{T}) where {Int <: T <: Integer} = 1
 
+# Piracy, but not for `ForeignType in treat_as_own`
+Base.findmax(::ForeignType, x::Int) = x + 1
+Base.findmax(::Set{Vector{ForeignType}}, x::Int) = x + 1
+Base.findmax(::Union{Foo,ForeignType}, x::Int) = x + 1
+
+# Piracy, but not for `ForeignParameterizedType in treat_as_own`
+Base.findmin(::ForeignParameterizedType{Int}, x::Int) = x + 1
+Base.findmin(::Set{Vector{ForeignParameterizedType{Int}}}, x::Int) = x + 1
+Base.findmin(::Union{Foo,ForeignParameterizedType{Int}}, x::Int) = x + 1
+
 # Assign them names in this module so they can be found by all_methods
-x = Base.findfirst
-y = Base.findlast
+a = Base.findfirst
+b = Base.findlast
+c = Base.findmax
+d = Base.findmin
 end # PiracyModule
 
 using Aqua: Piracy
+using PiracyForeignProject: ForeignType, ForeignParameterizedType
 
 # Get all methods - test length
 meths = filter(Piracy.all_methods(PiracyModule)) do m
@@ -55,9 +73,11 @@ end
 # 2 Foo constructors
 # 2 from f
 # 1 from MyUnion
-# 5 from findlast
+# 6 from findlast
 # 3 from findfirst
-@test length(meths) == 2 + 2 + 1 + 5 + 3
+# 3 from findmax
+# 3 from findmin
+@test length(meths) == 2 + 2 + 1 + 6 + 3 + 3 + 3
 
 # Test what is foreign
 BasePkg = Base.PkgId(Base)
@@ -70,8 +90,32 @@ ThisPkg = Base.PkgId(PiracyModule)
 @test !Piracy.is_foreign(Set{Int}, CorePkg)
 
 # Test what is pirate
-pirates = filter(Piracy.is_pirate, meths)
+pirates = filter(m -> Piracy.is_pirate(m), meths)
+@test length(pirates) == 3 + 3 + 3
+@test all(pirates) do m
+    m.name in [:findfirst, :findmax, :findmin]
+end
+
+# Test what is pirate (with treat_as_own=[ForeignType])
+pirates = filter(m -> Piracy.is_pirate(m; treat_as_own = [ForeignType]), meths)
+@test length(pirates) == 3 + 3
+@test all(pirates) do m
+    m.name in [:findfirst, :findmin]
+end
+
+# Test what is pirate (with treat_as_own=[ForeignParameterizedType])
+pirates = filter(m -> Piracy.is_pirate(m; treat_as_own = [ForeignParameterizedType]), meths)
+@test length(pirates) == 3 + 3
+@test all(pirates) do m
+    m.name in [:findfirst, :findmax]
+end
+
+# Test what is pirate (with treat_as_own=[ForeignType, ForeignParameterizedType])
+pirates = filter(
+    m -> Piracy.is_pirate(m; treat_as_own = [ForeignType, ForeignParameterizedType]),
+    meths,
+)
 @test length(pirates) == 3
 @test all(pirates) do m
-    m.name === :findfirst
+    m.name in [:findfirst]
 end
