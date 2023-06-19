b18fd2eed5d3b7b2d779d0f5f2f8e72f1dc824bb
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-06-19 16:40:34 +0200

    Change new test case to only run for julia v1.3+

--- test/pkgs/PkgWithAmbiguities.jl
+++ test/pkgs/PkgWithAmbiguities.jl
@@ -3,9 +3,11 @@ module PkgWithAmbiguities
 f(::Any, ::Int) = 1
 f(::Int, ::Any) = 2
 
-abstract type AbstractType{T} end
-struct ConcreteType{T} <: AbstractType{T} end
-(::AbstractType{T})(::Tuple{Tuple{Int}}) where {T} = 1
-(::ConcreteType)(::Tuple) = 2
+@static if VERSION >= v"1.3-"
+    abstract type AbstractType{T} end
+    struct ConcreteType{T} <: AbstractType{T} end
+    (::AbstractType{T})(::Tuple{Tuple{Int}}) where {T} = 1
+    (::ConcreteType)(::Tuple) = 2
+end
 
 end  # module
--- test/test_ambiguities.jl
+++ test/test_ambiguities.jl
@@ -5,23 +5,33 @@ include("preamble.jl")
 using PkgWithAmbiguities
 
 @testset begin
-    results = @testtestset begin
-        @info "↓↓↓ Following failures are expected. ↓↓↓"
-        Aqua.test_ambiguities(PkgWithAmbiguities)
+    @static if VERSION >= v"1.3-"
+        results = @testtestset begin
+            @info "↓↓↓ Following failures are expected. ↓↓↓"
+            Aqua.test_ambiguities(PkgWithAmbiguities)
 
-        # exclude just anything irrelevant, see #49
-        Aqua.test_ambiguities(PkgWithAmbiguities; exclude = [convert])
+            # exclude just anything irrelevant, see #49
+            Aqua.test_ambiguities(PkgWithAmbiguities; exclude = [convert])
 
-        Aqua.test_ambiguities(
-            PkgWithAmbiguities;
-            exclude = [PkgWithAmbiguities.f, PkgWithAmbiguities.AbstractType],
-        )
-        @info "↑↑↑ Above failures are expected. ↑↑↑" # move above once broken test fixed
+            Aqua.test_ambiguities(
+                PkgWithAmbiguities;
+                exclude = [PkgWithAmbiguities.f, PkgWithAmbiguities.AbstractType],
+            )
+            @info "↑↑↑ Above failures are expected. ↑↑↑" # move above once broken test fixed
+        end
+        @test length(results) == 3
+        @test results[1] isa Test.Fail
+        @test results[2] isa Test.Fail
+        @test_broken results[3] isa Test.Pass
+    else
+        results = @testtestset begin
+            @info "↓↓↓ Following failures are expected. ↓↓↓"
+            Aqua.test_ambiguities(PkgWithAmbiguities)
+            @info "↑↑↑ Above failures are expected. ↑↑↑"
+        end
+        @test length(results) == 1
+        @test results[1] isa Test.Fail
     end
-    @test length(results) == 3
-    @test results[1] isa Test.Fail
-    @test results[2] isa Test.Fail
-    @test_broken results[3] isa Test.Pass
 
     # It works with other tests:
     Aqua.test_unbound_args(PkgWithAmbiguities)
