629d44af80b554a521bfd7e57fb2f1e6a61a3521
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-06-24 15:33:59 +0200

    Allow non-singleton callables in `exclude`

--- test/pkgs/PkgWithAmbiguities.jl
+++ test/pkgs/PkgWithAmbiguities.jl
@@ -1,13 +1,40 @@
 module PkgWithAmbiguities
 
+# 1 ambiguity
 f(::Any, ::Int) = 1
 f(::Int, ::Any) = 2
 
+abstract type AbstractType end
+struct SingletonType <: AbstractType end
+
+struct ConcreteType <: AbstractType
+    x::Int
+end
+
+# 2 ambiguities
+SingletonType(::Any, ::Any, ::Int) = 1
+SingletonType(::Any, ::Int, ::Int) = 2
+SingletonType(::Int, ::Any, ::Any) = 3
+
+# 1 ambiguity
+(::SingletonType)(::Any, ::Float64) = 1
+(::SingletonType)(::Float64, ::Any) = 2
+
+# 3 ambiguities
+ConcreteType(::Any, ::Any, ::Int) = 1
+ConcreteType(::Any, ::Int, ::Any) = 2
+ConcreteType(::Int, ::Any, ::Any) = 3
+
+# 1 ambiguity
+(::ConcreteType)(::Any, ::Float64) = 1
+(::ConcreteType)(::Float64, ::Any) = 2
+
+
 @static if VERSION >= v"1.3-"
-    abstract type AbstractType{T} end
-    struct ConcreteType{T} <: AbstractType{T} end
-    (::AbstractType{T})(::Tuple{Tuple{Int}}) where {T} = 1
-    (::ConcreteType)(::Tuple) = 2
+    abstract type AbstractParameterizedType{T} end
+    struct ConcreteParameterizedType{T} <: AbstractParameterizedType{T} end
+    (::AbstractParameterizedType{T})(::Tuple{Tuple{Int}}) where {T} = 1
+    (::ConcreteParameterizedType)(::Tuple) = 2
 end
 
 end  # module
