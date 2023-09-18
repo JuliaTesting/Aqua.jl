46eec0987b054069f3976fd2bb8457f191b9d28d
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-09-19 01:01:58 +0200

    Print module in `test_undefined_exports` report (#177)
    * Add test
    
    * Print module in undefined export report

--- test/test_undefined_exports.jl
+++ test/test_undefined_exports.jl
@@ -5,6 +5,8 @@ include("preamble.jl")
 using PkgWithUndefinedExports
 
 @testset begin
+    @test Aqua.undefined_exports(PkgWithUndefinedExports) ==
+          [Symbol("PkgWithUndefinedExports.undefined_name")]
     results = @testtestset begin
         Aqua.test_undefined_exports(PkgWithUndefinedExports)
     end
