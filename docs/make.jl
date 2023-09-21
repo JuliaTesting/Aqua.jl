c1341bca1618a539260aa7443bb065975e22378f
Yuto Horikawa <hyrodium@gmail.com>
2023-09-21 22:27:39 +0900

    Update the verion of Documenter.jl to v1.0 (#191)

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,11 @@ makedocs(;
     modules = [Aqua],
     pages = ["Home" => "index.md"],
     sitename = "Aqua.jl",
+    format=Documenter.HTML(;
+        repolink = "https://github.com/JuliaTesting/Aqua.jl",
+    ),
     authors = "Takafumi Arakaki",
+    warnonly = true,
 )
 
 deploydocs(; repo = "github.com/JuliaTesting/Aqua.jl", push_preview = true)
