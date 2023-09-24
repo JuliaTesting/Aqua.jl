5801ae885f6aa33ee5879e6e792e49b7c05fa68a
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-09-24 22:37:01 +0200

    Let all VERSION switches use `@static`

--- src/pkg/Versions.jl
+++ src/pkg/Versions.jl
@@ -4,7 +4,7 @@ module Versions
 
 export VersionBound, VersionRange, VersionSpec, semver_spec, isjoinable
 
-if VERSION < v"1.1.0-DEV.472"
+@static if VERSION < v"1.1.0-DEV.472"
     using Compat: isnothing
 end
 
