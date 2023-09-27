b60e8a52864767c38238df6a03440fa9cf1c65be
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-09-28 00:26:31 +0200

    Enhance smoke testing

--- test/test_smoke.jl
+++ test/test_smoke.jl
@@ -1,18 +1,24 @@
 module TestSmoke
 
 using Aqua
+
+# test defaults
 Aqua.test_all(
     Aqua;
     stale_deps = (; ignore = [:Compat]),  # conditionally loaded
 )
+
+# test everything else
 Aqua.test_all(
     Aqua;
     ambiguities = false,
     unbound_args = false,
     undefined_exports = false,
-    stale_deps = (; ignore = [:Compat]),
-    deps_compat = true,
-    project_toml_formatting = true,
+    project_extras = false,
+    stale_deps = false,
+    deps_compat = false,
+    project_toml_formatting = false,
+    piracy = false,
 )
 
 end  # module
