9cfee8abc0e4af5f74592f1cc75e84d06ee74341
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-10-24 16:59:00 +0200

    Extend `test_ambiguity` tests by case with kwargs

--- test/test_ambiguities.jl
+++ test/test_ambiguities.jl
@@ -2,9 +2,28 @@ module TestAmbiguities
 
 include("preamble.jl")
 
-using PkgWithAmbiguities
-
 @testset begin
+    using PkgWithAmbiguities
+
+    @static if VERSION >= v"1.3-"
+        using PkgWithAmbiguities:
+            num_ambs_f,
+            num_ambs_g,
+            num_ambs_SingletonType,
+            num_ambs_ConcreteType,
+            num_ambs_ParameterizedType
+        total =
+            num_ambs_f +
+            num_ambs_g +
+            num_ambs_SingletonType +
+            num_ambs_ConcreteType +
+            num_ambs_ParameterizedType
+    else
+        using PkgWithAmbiguities:
+            num_ambs_f, num_ambs_g, num_ambs_SingletonType, num_ambs_ConcreteType
+        total = num_ambs_f + num_ambs_g + num_ambs_SingletonType + num_ambs_ConcreteType
+    end
+
     function check_testcase(exclude, num_ambiguities::Int; broken::Bool = false)
         pkgids = Aqua.aspkgids([PkgWithAmbiguities, Core]) # include Core to find constructor ambiguities
         num_ambiguities_, strout, strerr = Aqua._find_ambiguities(pkgids; exclude = exclude)
@@ -16,37 +35,45 @@ using PkgWithAmbiguities
         @test isempty(strerr)
     end
 
-    total = 9
-
-
     check_testcase([], total)
 
     # exclude just anything irrelevant, see #49
     check_testcase([convert], total)
 
     # exclude function
-    check_testcase([PkgWithAmbiguities.f], total - 1)
+    check_testcase([PkgWithAmbiguities.f], total - num_ambs_f)
+
+    # exclude function and kwsorter
+    check_testcase([PkgWithAmbiguities.g], total - num_ambs_g; broken = true)
 
     # exclude callables and constructors
-    check_testcase([PkgWithAmbiguities.SingletonType], total - 2 - 1)
-    check_testcase([PkgWithAmbiguities.ConcreteType], total - 3 - 1)
+    check_testcase([PkgWithAmbiguities.SingletonType], total - num_ambs_SingletonType)
+    check_testcase([PkgWithAmbiguities.ConcreteType], total - num_ambs_ConcreteType)
 
     # exclude abstract supertype without callables and constructors
     check_testcase([PkgWithAmbiguities.AbstractType], total)
 
     # for ambiguities between abstract and concrete type callables, only one needs to be excluded
-    check_testcase([PkgWithAmbiguities.AbstractParameterizedType], total - 1)
-    check_testcase([PkgWithAmbiguities.ConcreteParameterizedType], total - 1)
+    check_testcase(
+        [PkgWithAmbiguities.AbstractParameterizedType],
+        total - num_ambs_ParameterizedType,
+    )
+    check_testcase(
+        [PkgWithAmbiguities.ConcreteParameterizedType],
+        total - num_ambs_ParameterizedType,
+    )
 
     # exclude everything
     check_testcase(
         [
             PkgWithAmbiguities.f,
+            PkgWithAmbiguities.g,
             PkgWithAmbiguities.SingletonType,
             PkgWithAmbiguities.ConcreteType,
             PkgWithAmbiguities.ConcreteParameterizedType,
         ],
-        0,
+        0;
+        broken = true,
     )
 
     # It works with other tests:
