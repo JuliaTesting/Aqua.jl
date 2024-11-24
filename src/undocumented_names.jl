05bf3ec7fc127a8a8b0e92ab621edacb21139af3
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-24 12:44:21 +0100

    Filter names

--- src/undocumented_names.jl
+++ src/undocumented_names.jl
@@ -9,18 +9,19 @@ Test that all public names in `module` have a docstring (including the module it
 function test_undocumented_names(m::Module)
     @static if VERSION >= v"1.11"
         names = Docs.undocumented_names(m)
-        @test isempty(names) || (only(names) == nameof(m))
+        names_filtered = filter(n -> n != nameof(m), names)
+        @test isempty(names_filtered)
     else
-        names = Symbol[]
+        names_filtered = Symbol[]
     end
-    if !isempty(names)
+    if !isempty(names_filtered)
         printstyled(
             stderr,
             "Undocumented names detected:\n";
             bold = true,
             color = Base.error_color(),
         )
-        show(stderr, MIME"text/plain"(), names)
+        show(stderr, MIME"text/plain"(), names_filtered)
         println(stderr)
     end
 end
