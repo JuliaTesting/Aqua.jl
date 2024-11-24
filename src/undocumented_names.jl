2a3c4aa4ca79988914905451e14965e8674a0fe0
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-11-24 13:08:34 +0100

    Docstring

--- src/undocumented_names.jl
+++ src/undocumented_names.jl
@@ -1,7 +1,7 @@
 """
     test_undocumented_names(module::Module)
 
-Test that all public names in `module` have a docstring (including the module itself).
+Test that all public names in `module` have a docstring (not including the module itself).
 
 !!! warning
     For Julia versions before 1.11, this does not test anything.
