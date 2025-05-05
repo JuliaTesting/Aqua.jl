9c067ba0f8c8c8b4212f685c782e99c31043b938
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-05-05 15:59:04 +0200

    Undo testset wrapping

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,10 @@ module TestAqua
 
 using Test
 
-@testset verbose = true "Aqua" begin
-    @testset "$file" for file in sort([
-        file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
-    ])
-        include(file)
-    end
+@testset "$file" for file in sort([
+    file for file in readdir(@__DIR__) if match(r"^test_.*\.jl$", file) !== nothing
+])
+    include(file)
 end
 
 end  # module
