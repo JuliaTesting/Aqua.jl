91513f7f95d6c43c1e12939dad5b26093620304c
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2026-02-23 20:23:04 +0100

    feat!: test explicit imports using ExplicitImports.jl

--- test/test_smoke.jl
+++ test/test_smoke.jl
@@ -3,7 +3,50 @@ module TestSmoke
 using Aqua
 
 # test defaults
-Aqua.test_all(Aqua)
+
+# TODO: fix instead of ignoring
+private_explicit_imports_aqua = (:PkgId,) # Base
+private_explicit_imports_aquapiracy = (:is_in_mods,) # Test
+private_qualified_accesses_aqua = (
+    :JLOptions, # Base
+    :PkgId, # Base
+    :SIGKILL, # Base
+    :error_color, # Base
+    :eval, # Base
+    :isbindingresolved, # Base
+    :isdeprecated, # Base
+    :julia_cmd, # Base
+    :kwcall, # Core
+    :kwftype, # Core
+    :load_path_setup_code, # Base
+    :morespecific, # Base
+    :require, # Base
+    :respect_sysimage_versions, # Pkg
+    :show_tuple_as_call, # Base
+    :unwrap_unionall, # Base,
+)
+private_qualified_accesses_aquapiracy = (
+    :MethodTable, # Core
+    :TypeofVararg, # Core
+    :loaded_modules_array, # Base
+    :methodtable, # Core
+    :uniontypes, # Base
+    :visit, # Base
+)
+
+explicit_imports_param_for_aqua = (;
+    all_explicit_imports_are_public = (;
+        ignore = (private_explicit_imports_aqua..., private_explicit_imports_aquapiracy...)
+    ),
+    all_qualified_accesses_are_public = (;
+        ignore = (
+            private_qualified_accesses_aqua...,
+            private_qualified_accesses_aquapiracy...,
+        )
+    ),
+)
+
+Aqua.test_all(Aqua; explicit_imports = explicit_imports_param_for_aqua)
 
 # test everything else
 Aqua.test_all(
@@ -16,6 +59,8 @@ Aqua.test_all(
     deps_compat = false,
     piracies = false,
     persistent_tasks = false,
+    undocumented_names = false,
+    explicit_imports = false,
 )
 
 end  # module
